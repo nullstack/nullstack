04c958792964df92a0886f91b493386f3469e01a
Christian Mortaro <christian.mortaro@gmail.com>
2019-09-05 07:19:26 -0300

    version bump

--- template/index.js
+++ template/index.js
@@ -2,10 +2,17 @@ import './styles.scss';
 
 import {route, config, ready} from 'nullstack';
 
-config('database.url', 'mongodb://localhost:27017/{{PROJECT_NAME}}');
-config('database.name', '{{PROJECT_NAME}}');
-config('session.secret', '{{SESSION_SECRET}}');
-config('server.port', 3769);
+config('database.url', 'mongodb://localhost:27017/{{PROJECT_NAME}}'); //server
+config('database.name', '{{PROJECT_NAME}}'); //server
+config('session.secret', '{{SESSION_SECRET}}'); //server
+config('server.port', 3769); //server
+
+config('storage.type', 'disk'); //server
+config('storage.key', 'KEY'); //server
+config('storage.secret', 'SECRET'); //server
+config('storage.region', 'sa-east-1'); //server
+config('storage.bucket', 'BUCKET'); //server
+
 config('default.description', 'TODO');
 config('default.project', 'TODO');
 config('default.image', 'TODO');
