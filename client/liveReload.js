175997a10bd78d0961caae257208bf9481a886ec
Christian <christian.mortaro@gmail.com>
2021-03-17 22:23:12 -0300

    :bug: fix firefox livereload

--- client/liveReload.js
+++ client/liveReload.js
@@ -1,12 +1,22 @@
 let shouldReloadNext = false;
+let timer = null;
+
+function reload() {
+  if(shouldReloadNext) {
+    clearInterval(timer);
+    timer = setTimeout(() => {
+      location.reload()
+      console.log('reload');
+    }, 10)
+  } else {
+    shouldReloadNext = true;
+  }
+}
 
 function liveReload() {
   const socket = new WebSocket('ws://' + location.host);
-  socket.addEventListener('open', () => {
-    shouldReloadNext && location.reload();
-    shouldReloadNext = true;
-  });
+  socket.addEventListener('open', reload);
   socket.addEventListener('close', liveReload);
 }
 
-liveReload()
+liveReload();
