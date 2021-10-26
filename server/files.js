ec13a434108d03ab3fb9d247fe219b9a1005c5f2
Mortaro <christian.mortaro@gmail.com>
2021-10-25 23:42:10 -0300

    :bookmark: allow empty css

--- server/files.js
+++ server/files.js
@@ -1,14 +1,19 @@
-import {readFileSync} from 'fs';
+import { readFileSync, existsSync } from 'fs';
 import environment from './environment';
 import path from 'path';
-import {generateIntegrity} from './integrities';
+import { generateIntegrity } from './integrities';
 
 const files = {};
 
 export function generateFile(file, server) {
-  if(files[file] && environment.production) return files[file];
-  files[file] = readFileSync(path.join(__dirname, file), 'utf-8');
-  if(!server.less) {
+  if (files[file] && environment.production) return files[file];
+  const filePath = path.join(__dirname, file)
+  if (existsSync(filePath)) {
+    files[file] = readFileSync(filePath, 'utf-8');
+  } else {
+    files[file] = ''
+  }
+  if (!server.less) {
     generateIntegrity(file, files[file]);
   }
   return files[file];
