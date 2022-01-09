ced37e0583c44ed4950821695af5d575df753780
Mortaro <christian.mortaro@gmail.com>
2022-01-09 19:49:48 -0300

    :white_check_mark: tests for tailwind 3 purge

---
+++ tests/src/Purge.test.js
@@ -0,0 +1,31 @@
+const { readFileSync } = require('fs');
+
+let css;
+
+beforeAll(async () => {
+  css = readFileSync('.production/client.css', 'utf-8')
+});
+
+describe('.production', () => {
+
+  test('used classes stay after purge', async () => {
+    const hasClass = css.includes('.class')
+    expect(hasClass).toBeTruthy();
+  })
+
+  test('used classes with brackets stay after purge', async () => {
+    const hasClass = css.includes('.class-\[custom\]')
+    expect(hasClass).toBeTruthy();
+  })
+
+  test('used classes with dots stay after purge', async () => {
+    const hasClass = css.includes('.class-0\.5')
+    expect(hasClass).toBeTruthy();
+  })
+
+  test('unused classes are removed during purge', async () => {
+    const hasClass = css.includes('.unused')
+    expect(hasClass).toBeFalsy();
+  })
+
+})
