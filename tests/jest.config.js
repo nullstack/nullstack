0d77ac97153855c84a8fa8367fbb3e69b4c813f5
GuiDevloper <guicorrea384@gmail.com>
2022-01-20 19:17:57 -0300

    :green_heart: Remove the need for a jest-environment

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -2,7 +2,6 @@ const CI = Boolean(process.env.CI || false);
 
 module.exports = {
   preset: "jest-puppeteer",
-  testEnvironment: "./jest-environment.js",
   forceExit: CI,
   testTimeout: CI ? 5000 : 20000
 }
