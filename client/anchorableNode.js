1f052d02a5c2b30066c8721dcdc2b2c7f1a51d41
Mortaro <christian.mortaro@gmail.com>
2022-02-15 21:35:36 -0300

    :construction: innerhtml anchor changes

--- client/anchorableNode.js
+++ client/anchorableNode.js
@@ -2,9 +2,9 @@ import router from './router'
 
 export function anchorableElement(element) {
   const links = element.querySelectorAll('a[href^="/"]:not([target])')
-  if (element.dataset.nullstack) return
-  element.dataset.nullstack = true
   for (const link of links) {
+    if (link.dataset.nullstack) return
+    link.dataset.nullstack = true
     link.addEventListener('click', (event) => {
       if (!event.ctrlKey && !event.shiftKey && !event.altKey) {
         event.preventDefault()
