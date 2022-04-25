3db53da0a5d25b97ac28b6b313cacf6b8d06649a
GuiDevloper <guicorrea384@gmail.com>
2022-04-25 02:30:47 -0300

    :heavy_minus_sign: Replace unneeded deps by local or internal

---
+++ loaders/string-replace.js
@@ -0,0 +1,20 @@
+/*
+Copyright (c) 2015 Valentyn Barmashyn
+MIT Licensed
+Original: https://npmjs.com/package/string-replace-loader
+*/
+module.exports = function(source, map) {
+  this.cacheable();
+
+  const optionsArray = this.getOptions().multiple;
+  let newSource = source;
+
+  for (const options of optionsArray) {
+    newSource = newSource.replace(
+      new RegExp(options.search, options.search.flags || options.flags || ''),
+      options.replace
+    );
+  }
+
+  this.callback(null, newSource, map);
+}
