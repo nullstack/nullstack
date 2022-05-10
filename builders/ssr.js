c9a51dba174750ed92cdfecc1656e8b11ca3b25f
GuiDevloper <guicorrea384@gmail.com>
2022-05-10 04:39:33 -0300

    :technologist: Default project name in builders

--- builders/ssr.js
+++ builders/ssr.js
@@ -1,8 +1,9 @@
 module.exports = async function ssr({ cache }) {
   const dir = process.cwd();
   const application = require(`${dir}/.production/server`).default;
+  const projectName = application.project.name || 'The app';
   
-  console.log('\x1b[36m%s\x1b[0m', `\n ✅️ ${application.project.name} is ready for production\n`);
+  console.log('\x1b[36m%s\x1b[0m', `\n ✅️ ${projectName} is ready for production\n`);
 
   if (cache) {
     console.log('Storing cache...');
