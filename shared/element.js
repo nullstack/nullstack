8395790953b28171bb37ffb63023aca18a74894a
Christian Mortaro <christian.mortaro@gmail.com>
2022-05-14 05:58:01 -0300

    :zap: faster element

--- shared/element.js
+++ shared/element.js
@@ -1,25 +1,23 @@
 import fragment from './fragment';
 
-function flattenChildren(children) {
-  children = [].concat.apply([], children).map((child) => {
-    if(child === null || child === undefined) return false;
-    return child;
-  });
-  return [].concat.apply([], children);
+const seed = Object.freeze([])
+
+function normalize(child) {
+  return child ?? false
 }
 
 export default function element(type, props, ...children) {
-  children = flattenChildren(children);
-  if(type === 'textarea') {
+  children = seed.concat(...children).map(normalize)
+  if (type === 'textarea') {
     children = [children.join('')];
   }
-  const attributes = {...props, children};
-  if(type === 'element') {
+  const attributes = { ...props, children };
+  if (type === 'element') {
     type = attributes.tag || fragment;
     delete attributes.tag;
   }
-  if(typeof(type) === 'function' && type.render !== undefined) {
-    return {type, attributes, children: null}
+  if (typeof type === 'function' && type.render !== undefined) {
+    return { type, attributes, children: null }
   }
-  return {type, attributes, children};
+  return { type, attributes, children };
 }
